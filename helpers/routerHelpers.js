19c135cfd598f3f61151897ee555237d3bc9f514
Le Anh Huy <vrinv123456@gmail.com>
2020-12-04 21:22:10 +0700

    Second Commit

--- helpers/routerHelpers.js
+++ helpers/routerHelpers.js
@@ -69,7 +69,8 @@ const schemas = {
     userSchema: Joi.object().keys({
         firstName: Joi.string().min(2).required(),
         lastName: Joi.string().min(2).required(),
-        email: Joi.string().email().required()
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required()
     }),
 
     userOptionalSchema: Joi.object().keys({
